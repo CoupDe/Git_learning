6fcdb018456ed91df5636fa426ef2e2908fb955e
CoupDe <coupdegr@gmail.com>
2021-05-26 17:39:20 +0300

    JSON Аргумент indent =4

--- Type/File_lesson/JSON_lesson.py
+++ Type/File_lesson/JSON_lesson.py
@@ -2,5 +2,8 @@ import json
 
 employee = dict(name='Artem', age=35, child=['Eva', 'Artemovna'], wife=['Daria', 'Sergeevna', 'Alekseeva'])
 
-with open('JSON.json', 'a') as newJson:
-    json.dump(employee, newJson, indent=4)
+with open('JSON.json', 'w') as newJson:
+    json.dump(employee, newJson, indent=4)  # indent - создает отступы
+newJson.close()
+
+print(json.load(open('JSON.json')))
