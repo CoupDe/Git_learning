5a99c7c31c42a68c6699b42abe227401c17d6da7
CoupDe <coupdegr@gmail.com>
2021-05-27 08:23:42 +0300

    Backup

--- Type/File_lesson/JSON_lesson.py
+++ Type/File_lesson/JSON_lesson.py
@@ -1,10 +1,12 @@
 import json
 
-employee = dict(name='Artem', age=35, child=['Eva', 'Artemovna'], wife=['Daria', 'Sergeevna', 'Alekseeva'])
+employee = dict(name='Artem', age=35, child=['Eva', 'Artemovna'], wife=dict(
+    name='Daria', secondname='Sergeevna', lastname='Alekseeva'))
 
 with open('JSON.json', 'w') as newJson:  # Диспетчер контекста гарантирует закрыте файла
     json.dump(employee, newJson, indent=4)  # indent - создает отступы
 
 if newJson.closed:
     print('is closed')
-print(json.load(open('JSON.json')))
+loadDB = json.load(open('JSON.json'))
+print(loadDB['wife']['lastname'])
